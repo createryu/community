bd797770c952b2b5006b3f1d3d6c791605da1f75
createryu <714770145@qq.com>
2020-03-19 10:18:21 +0800

    init repo

---
+++ src/main/java/com/yuqiliu/community/niuliucommunity/NiuliuCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.yuqiliu.community.niuliucommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiuliuCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiuliuCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuqiliu/community/niuliucommunity/NiuliuCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuqiliu.community.niuliucommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiuliuCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
